9cae0411d1deb32c246103122573b7de57c5bdf8
Glitch (simple-address-search) <none>
2020-02-06 23:32:24 +0000

    search feature layer

--- js/script.js
+++ js/script.js
@@ -1,35 +1,56 @@
-require([
-      "esri/Map",
-      "esri/views/SceneView",
-      "esri/widgets/Search"
-    ], function(
-      Map,
-      SceneView,
-      Search) {
+require(["esri/Map", 
+         "esri/views/SceneView", 
+         "esri/layers/FeatureLayer",
+         "esri/widgets/Search"], function(
+  Map,
+  SceneView,
+  FeatureLayer,
+  Search
+) {
+  var map = new Map({
+    basemap: "satellite",
+    ground: "world-elevation"
+  });
 
-      var map = new Map({
-        basemap: "satellite",
-        ground: "world-elevation"
-      });
+  var view = new SceneView({
+    scale: 123456789,
+    container: "viewDiv",
+    map: map,
+    popup: {
+      actions: [],
+      dockEnabled: true,
+      dockOptions: {
+        buttonEnabled: true,
+        breakpoint: false,
+        position: "bottom-right"
+      }
+    }
+  });
+  
+  var featureLayerDistricts = new FeatureLayer({
+          url:
+            "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/CongressionalDistricts/FeatureServer/0",
+          popupTemplate: {
+            // autocasts as new PopupTemplate()
+            title: "Congressional District {DISTRICTID} </br>{NAME},{PARTY}",
+            overwriteActions: true
+          }
+        });
+  
 
-      var view = new SceneView({
-        scale: 123456789,
-        container: "viewDiv",
-        map: map,
-        popup: {
-           actions: [],
-           dockEnabled: true,
-           dockOptions: {
-             buttonEnabled: true,
-             breakpoint: false,
-             position: "bottom-right",
-           }
-         }
-      });
-
-      var searchWidget = new Search({
-        view: view,
-        container: "searchDiv"
-      });
-
-    });
+  var searchWidget = new Search({
+    view: view,
+    container: "searchDiv",
+    sources: [
+            {
+              layer: featureLayerDistricts,
+              searchFields: ["DISTRICTID"],
+              displayField: "DISTRICTID",
+              exactMatch: false,
+              outFields: ["DISTRICTID", "NAME", "PARTY"],
+              name: "Congressional Districts",
+              placeholder: "example: 3708"
+            }
+      ]
+  });
+});
